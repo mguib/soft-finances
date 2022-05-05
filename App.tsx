d090421e65d7c2ef467eac8d7da9842fc16f2f30
guibson <mgss.lic17@uea.edu.br>
2022-05-05 16:31:47 -0300

    feat: Adicionando HighlightCard

--- App.tsx
+++ App.tsx
@@ -1,14 +1,32 @@
 
 import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
+import{
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+
+import  theme  from './src/global/styles/theme';
 
 import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
-  return (
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
 
-    <Dashboard />
-
-
-  );
+  if(!fontsLoaded){
+    return <AppLoading/>
+  }else{
+    return (
+      <ThemeProvider theme={theme}>
+        <Dashboard/>
+      </ThemeProvider>    
+    );
+  }
 }
-
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,13 +1,46 @@
 import React from "react";
-import {Container, Title} from './styles'
+import { HighlightCard } from "../../components/HighlightCard";
 
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon,
+    HighlightCards
+
+} from './styles'
 
 export function Dashboard(){
     return(
         <Container>
-            <Title>
-                Dashboard
-            </Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{uri:'https://avatars.githubusercontent.com/u/43528733?v=4'}}/>
+
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Guib</UserName>
+                        </User>
+                    </UserInfo>
+
+                    <Icon name="power"/>
+                </UserWrapper>
+            </Header>
+
+            <HighlightCards
+                
+            >
+                <HighlightCard/>
+                <HighlightCard/>
+                <HighlightCard/>
+            </HighlightCards>
+            
         </Container>
     )
 }
