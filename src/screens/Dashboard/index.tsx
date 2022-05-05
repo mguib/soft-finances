f2fed7b991f30b70ad493d420ebca7d1ebb82cac
guibson <mgss.lic17@uea.edu.br>
2022-05-05 20:31:10 -0300

    feat: Adicionando estilos a listagem das trasanctionsCard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { HighlightCard } from "../../components/HighlightCard";
-import { TransactionCard } from "../../components/TransactionCard";
+import { TransactionCard, TransactionCardProps } from "../../components/TransactionCard";
 
 import {
     Container,
@@ -16,10 +16,52 @@ import {
     HighlightCards,
     Transactions,
     Title,
+    TransactionList,
 
 } from './styles'
 
+export interface DataListProps extends TransactionCardProps{
+    id:string;
+}
+
 export function Dashboard(){
+    const data: DataListProps[] = [
+    {
+        id: '1',
+        type:'positive',
+        title :"Desenvolvimento de site",
+        amount:"R$ 12.000,00",
+        category:{
+            name: 'Vendas',
+            icon:'dollar-sign'
+        },
+        date: "03/12/2021"
+    },
+    {
+        id: '2',
+        type:'negative',
+        title :"Hamburgueria Pizzy",
+        amount:"R$ 80,00",
+        category:{
+            name: 'Alimentação',
+            icon:'coffee'
+        },
+        date: "12/11/2021"
+    },
+    {
+        id: '3',
+        type:'negative',
+        title :"Aluguel do AP",
+        amount:"R$ 1.200,00",
+        category:{
+            name: 'Casa',
+            icon:'shopping-bag'
+        },
+        date: "11/07/2021"
+    },
+];
+    
+
     return(
         <Container>
             <Header>
@@ -62,8 +104,14 @@ export function Dashboard(){
 
             <Transactions>
                 <Title>Listagem</Title>
+
+                <TransactionList 
+                    data={data}
+                    keyExtractor={item => item.id}
+                    renderItem= {({ item }) => <TransactionCard data={item} />}
+                    
+                />                
                 
-                <TransactionCard/>
             </Transactions>
             
         </Container>
