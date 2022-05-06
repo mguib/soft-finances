28b6db5206c6b1b73e3cb375b28626fb0ca32f92
guibson <mgss.lic17@uea.edu.br>
2022-05-06 13:23:44 -0300

    feat: Adicioando formulário de cadastro usando reat-roock-form

--- App.tsx
+++ App.tsx
@@ -11,7 +11,9 @@ import{
 
 import  theme  from './src/global/styles/theme';
 
-import { Dashboard } from './src/screens/Dashboard';
+// import { Dashboard } from './src/screens/Dashboard';
+import { Register } from './src/screens/Register';
+import { CategorySelect } from './src/screens/CategorySelect';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,7 +27,8 @@ export default function App() {
   }else{
     return (
       <ThemeProvider theme={theme}>
-        <Dashboard/>
+        <Register/>
+        {/* <CategorySelect/> */}
       </ThemeProvider>    
     );
   }
