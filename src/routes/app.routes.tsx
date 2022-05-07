2bd34717c96f32e1348f3312d298df6d40557193
guibson <mgss.lic17@uea.edu.br>
2022-05-06 23:16:17 -0300

    feat: Adicionando estilização no TabNavigation

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { useTheme } from "styled-components";
+import { Platform } from "react-native";
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {MaterialIcons} from '@expo/vector-icons';
 
 const {Navigator, Screen} = createBottomTabNavigator();
 
@@ -8,19 +11,59 @@ import {Register} from '../screens/Register'
 
 
 export function AppRoutes(){
+    const theme = useTheme();
+
     return(
-        <Navigator>
+        <Navigator
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: theme.colors.secundary,
+                tabBarInactiveTintColor: theme.colors.text,
+                tabBarLabelPosition: 'beside-icon',
+                tabBarStyle: {
+                    height: 60,
+                    paddingVertical: Platform.OS ==='ios' ? 20 : 0,
+                }
+            }}
+        >
             <Screen
                 name="Listagem"
                 component={Dashboard}
+                options={{
+                    tabBarIcon: (({size, color}) => 
+                        <MaterialIcons
+                            name="format-list-bulleted"
+                            size={size}
+                            color= {color}
+                        />
+                    )
+                }}
             />
             <Screen
                 name="Cadastrar"
                 component={Register}
+                options={{
+                    tabBarIcon: (({size, color}) => 
+                        <MaterialIcons
+                            name="attach-money"
+                            size={size}
+                            color= {color}
+                        />
+                    )
+                }}
             />
             <Screen
                 name="Resumo"
                 component={Register}
+                options={{
+                    tabBarIcon: (({size, color}) => 
+                        <MaterialIcons
+                            name="pie-chart"
+                            size={size}
+                            color= {color}
+                        />
+                    )
+                }}
             />
         </Navigator>
     )
